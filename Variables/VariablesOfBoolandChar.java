2dbbdb72e44fef227eb605760104226f1a3d4c88
burakTuran00 <burakturan7410@gmail.com>
2023-08-13 18:22:03 +0300

    Java

---
+++ Variables/VariablesOfBoolandChar.java
@@ -0,0 +1,15 @@
+public class VariablesOfBoolandChar {
+    public static void main(String[] args) {
+
+        char a = 'A';
+        char b = '%';
+        char c = 1000;
+
+        System.out.println(c);
+
+        boolean isAvailable = true;
+        boolean isThereBullet = false;
+        
+        System.out.println(isAvailable);
+    }
+}
