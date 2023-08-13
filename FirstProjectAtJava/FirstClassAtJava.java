663799d7c359492a629ae676d14753161cd0521e
burakTuran00 <burakturan7410@gmail.com>
2023-08-13 15:01:38 +0300

    FirstProjectAtJava

---
+++ FirstProjectAtJava/FirstClassAtJava.java
@@ -0,0 +1,10 @@
+public class FirstClassAtJava {
+    public static void main(String[] args) {
+
+        System.out.print("Hello");
+        System.out.println("Hello World!");
+        System.out.println("I came from Java.");
+        System.out.println("When you're typing 'sout', it'll come direct 'System.out.println'");
+        // Typing "sout" means that System.out.println();
+    }
+}
