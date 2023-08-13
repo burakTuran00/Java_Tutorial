40dd7514dabead530198a467d16215a2c803c6cc
burakTuran00 <burakturan7410@gmail.com>
2023-08-13 17:41:50 +0300

    Java

--- Variables/VariablesOfJava.java
+++ Variables/VariablesOfJava.java
@@ -1,7 +1,22 @@
 public class VariablesOfJava {
-
     public static void main(String[] args) {
+        int a;
+        a = 10;
 
-        
+        int b = 12;
+        int c = 2, d = 5;
+
+        int x = (a + b) / c;
+        int sum = a + b + c + d;
+
+        System.out.println("Sum is "+ sum);
+
+        // double slash means that is a command line. The program don't write and execute this line.
+
+        int m = 20;
+        int n = m * 2;
+        int mn = n - 5;
+
+        System.out.println(mn);
     }
 }
