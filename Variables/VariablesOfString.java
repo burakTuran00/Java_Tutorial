e0b0b97e823322fe188c1dbb7cb17a9dce847b37
burakTuran00 <burakturan7410@gmail.com>
2023-08-13 19:49:38 +0300

    Java

---
+++ Variables/VariablesOfString.java
@@ -0,0 +1,20 @@
+public class VariablesOfString {
+    public static void main(String[] args) {
+        String name = "Rick";
+        System.out.println("Hello " + name + "!");
+
+        String a = "Java";
+        String b = "Programming";
+        String c = "Language";
+
+        String sum = a + b + c;
+
+        System.out.println(sum);
+
+        String x = "Examine";
+        int y = 45;
+
+        String z = x + y;
+        System.out.println(z);
+    }
+}
