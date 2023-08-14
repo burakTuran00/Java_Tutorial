a17e00f4429cc232d21ea87990cf4cd89cc65252
burakTuran00 <burakturan7410@gmail.com>
2023-08-14 10:27:33 +0300

    Java

---
+++ Variables/AdjustTaxiPrice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AdjustTaxiPrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How much does your car burn per kilometer:");
+        double cost = scanner.nextDouble();
+
+        System.out.print("How many kilometer are you going:");
+        int kilometer = scanner.nextInt();
+
+        System.out.println("The total amount of your cost is about "+ (cost * kilometer)+ "$");
+    }
+}
