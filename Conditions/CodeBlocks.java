97eb8c829a43dafe64ff5e4ec2ab57ee84d8f264
burakTuran00 <burakturan7410@gmail.com>
2023-08-15 11:33:47 +0300

    Java

---
+++ Conditions/CodeBlocks.java
@@ -0,0 +1,5 @@
+public class CodeBlocks {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Conditions/ComparisonOperators.java
@@ -0,0 +1,12 @@
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        /*
+        == --> equal
+        != --> not equal
+        >  --> greater
+        > --> less
+        >= --> greater or equal
+        <= --> less or equal
+         */
+    }
+}
