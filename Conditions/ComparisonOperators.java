e97df048c6a479bdc794fd4b112ab7ef8089c1e0
burakTuran00 <burakturan7410@gmail.com>
2023-08-15 11:50:49 +0300

    Java

--- Conditions/ComparisonOperators.java
+++ Conditions/ComparisonOperators.java
@@ -8,5 +8,9 @@ public class ComparisonOperators {
         >= --> greater or equal
         <= --> less or equal
          */
+
+        System.out.println(5 != 2); // ture
+        System.out.println(4 > 5);  // false
+        System.out.println(3 <= 3); // ture
     }
 }
---
+++ Conditions/LogicalOperators.java
@@ -0,0 +1,12 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        /*
+        && --> and ( if all of the conditions are true, then it'll return true.)
+        || --> or  ( if one of the conditions are true, then it'll return true.)
+        !  --> not ( true -> false , false -> true)
+         */
+
+        System.out.println(3 == 3 && 2 != 3); // Both condition is true, then true.
+        System.out.println(1 != 1 || 3 > 1); // One condition is ture, then true.
+    }
+}
