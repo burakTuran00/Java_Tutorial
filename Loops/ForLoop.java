a3c4631951e0fa06e1442aa98a4d413d5cef49b1
burakTuran00 <burakturan7410@gmail.com>
2023-08-16 17:14:57 +0300

    Java

--- Loops/ForLoop.java
+++ Loops/ForLoop.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class ForLoop {
     public static void main(String[] args) {
 
-        for (int i= 0; i < 10; i++)
+        /*for (int i= 0; i < 10; i++)
         {
             System.out.println(i);
         }
@@ -10,5 +12,36 @@ public class ForLoop {
         {
             System.out.println(i);
         }
+
+
+        int j = 0 ;
+
+        for (; j <= 5; j++)
+        {
+            System.out.println("Different using");
+        }
+
+        int x = 0;
+        int y = 10;
+
+        for (; x < 10 && y > 0; x++,y--)
+        {
+            System.out.println("x="+x);
+            System.out.println("y="+y);
+        }*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number for calculating factorial:");
+        int number = scanner.nextInt();
+        int factorial=1;
+
+        for (int i =1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        System.out.println("The factorial of "+ number +" is equal to "+factorial);
+
     }
 }
