9b4a4bb188769f51810317cc801270770714a866
burakTuran00 <burakturan7410@gmail.com>
2023-08-17 10:42:21 +0300

    Java

---
+++ Loops/UserEnterProgram.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class UserEnterProgram {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String systemUserName = "hello";
+        String systemPassword = "123";
+
+        while (true)
+        {
+            System.out.print("Enter Username:");
+            String username = scanner.nextLine();
+
+            System.out.print("Enter Password:");
+            String password = scanner.nextLine();
+
+            if (username.equals(systemUserName) && password.equals(systemPassword))
+            {
+                System.out.println("You are in!");
+                break;
+            }
+            else
+            {
+                System.out.println("Something is wrong!\nPlease try again!");
+            }
+        }
+    }
+}
