e9a32c33d96a6b5369e19f0ad0d8c7108c4b083f
burakTuran00 <burakturan7410@gmail.com>
2023-08-18 15:47:22 +0300

    Java

---
+++ Methods/Parameters.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Parameters
+{
+
+    private static void Addition(int number1, int number2, int number3)
+    {
+        int result = number1 + number2 + number3;
+        System.out.println("The result:"+result);
+    }
+    public static void main(String[] args)
+    {
+        Addition(1,2,3);
+        Addition(10,10,10 );
+    }
+
+}
---
+++ Methods/ReturnInMethods.java
@@ -0,0 +1,17 @@
+public class ReturnInMethods {
+    private static int Addition(int a, int b, int c)
+    {
+        return (a+b+c);
+    }
+    public static int ToMultipleTwo(int a)
+    {
+        return 2 * a;
+    }
+    public static void main(String[] args)
+    {
+        int resutl = Addition(1,2,3); // 6
+        System.out.println(resutl + Addition(1,2,3)); // 6 + 6 = 12
+
+
+    }
+}
