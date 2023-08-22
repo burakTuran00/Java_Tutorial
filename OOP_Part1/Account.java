b543bab538f8b6e7e5d17af3c5ea54eb095bd7e7
burakTuran00 <burakturan7410@gmail.com>
2023-08-22 13:14:35 +0300

    Java

---
+++ OOP_Part1/Account.java
@@ -0,0 +1,8 @@
+public class Account {
+     private String account_No;
+     private double account_Balance;
+     private String account_OwnerName;
+     private String account_Mail;
+     private String account_TelNo;
+
+}
