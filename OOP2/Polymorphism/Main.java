dd8ce09177e2edeb32d646a74774b89539c6dad8
burakTuran00 <burakturan7410@gmail.com>
2023-08-26 11:25:29 +0300

    Java

--- OOP2/Polymorphism/Main.java
+++ OOP2/Polymorphism/Main.java
@@ -58,23 +58,66 @@ class Horse extends Animal
     }
 }
 
-
-
 public class Main {
 
-    public static void TalkFunc(Animal animal)
+    public static void Talkf(Animal animal)
     {
         System.out.println(animal.Talk());
     }
+    public static void TalkFunc(Object object)
+    {
+        // aganist to polymorphism, use it.
+
+        if (object instanceof Dog)
+        {
+            Dog dog = (Dog) object;
+            System.out.println(dog.Talk());
+        }
+        else if(object instanceof Cat)
+        {
+            Cat cat = (Cat) object;
+            System.out.println(cat.Talk());
+        }
+        else if(object instanceof Horse)
+        {
+            Horse horse = (Horse) object;
+            System.out.println(horse.Talk());
+        }
+        else if(object instanceof Animal)
+        {
+            Animal animal = (Animal)  object;
+            System.out.println(animal.Talk());
+        }
+    }
+
     public static void main(String[] args)
     {
-        TalkFunc(new Cat("Ole"));
-        TalkFunc(new Dog("Petes"));
+        Talkf(new Cat("Ole"));
+        Talkf(new Dog("Petes"));
 
         Animal animal = new Horse("Hara");
         System.out.println(animal.Talk());
 
         Animal animal1 = new Dog("Doggi");
         System.out.println(animal1.Talk());
+
+        Dog dog = new Dog("Karabaş");
+
+        if (dog instanceof  Animal)
+        {
+            System.out.println(true);
+        }
+
+
+
+       Dog dog = new Dog("karabaş");
+        Cat cat = new Cat("tekir");
+        Horse horse = new Horse("yaman");
+        Animal animal = new Animal("Turp");
+
+        TalkFunc(dog);
+        TalkFunc(cat);
+        TalkFunc(horse);
+        TalkFunc(animal);
     }
 }
