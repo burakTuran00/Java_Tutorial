21791f1b91339824c5fb17e283f92a40eae6018f
burakTuran00 <burakturan7410@gmail.com>
2023-08-28 20:33:17 +0300

    Java

---
+++ Arrays/ForeachLoop.java
@@ -0,0 +1,11 @@
+public class ForeachLoop
+{
+    public static void main(String[] args) {
+        String[] arr = {"Apple","Banana","Peace"};
+
+        for (String x : arr)
+        {
+            System.out.println(x);
+        }
+    }
+}
