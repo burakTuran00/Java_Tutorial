213092f886cf87854e22c07e59a9d4859b96f152
burakTuran00 <burakturan7410@gmail.com>
2023-09-03 09:12:29 +0300

    Java

--- LocalInnerClass/src/Main.java
+++ LocalInnerClass/src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        class Area
+        {
+            public void CircleArea(int radius)
+            {
+                System.out.println("Area of Circle:" + (radius * radius * Math.PI));
+            }
+        }
+
+        Area area = new Area();
+        Area area1 = new Area();
+
+        area.CircleArea(2);
+        area.CircleArea(12);
+    }
+
+    public static void Try()
+    {
+        // we can't use Area class because of it is special for in that class!
     }
 }
+
+
+/* Benefits of Local Inner Class
+-need immediately to create object. don't need tp create other
+-just using in that function
+ */
