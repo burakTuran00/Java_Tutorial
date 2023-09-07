77df602c056654794840a5e9d147600bbf85bbac
burakTuran00 <burakturan7410@gmail.com>
2023-09-07 12:07:29 +0300

    Java

---
+++ InterfaceDetails/src/IDeneme.java
@@ -0,0 +1,12 @@
+public interface IDeneme
+{
+    public int a = 5; // must be public
+    public String name = "Roto";
+
+    public final double pi = 3.14;  // final is not necessary
+
+    public static int Sum(int a , int b) // must be static
+    {
+        return  a + b;
+    }
+}
---
+++ InterfaceDetails/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println(IDeneme.a);
+        // we can access ont name of interface
+        System.out.println(IDeneme.name);
+
+        System.out.println(IDeneme.Sum(1,1));
+    }
+}
